f6d2e94e4a719edc2c63917315c9a87351b95f72
Marlon Secundo <marlon_secundo@outlook.com>
2019-07-19 10:21:19 -0300

    Championship Filter Updated

--- app/ModelFilters/ChampionshipFilter.js
+++ app/ModelFilters/ChampionshipFilter.js
@@ -1,9 +1,27 @@
-'use strict'
+'use strict';
 
-const ModelFilter = use('ModelFilter')
+const ModelFilter = use('ModelFilter');
 
 class ChampionshipFilter extends ModelFilter {
+  sex(sex) {
+    return this.where({ sex });
+  }
 
+  type(type) {
+    return this.where({ type });
+  }
+
+  upperLimit(upperLimit) {
+    return this.where({ upperLimit });
+  }
+
+  downLimit(downLimit) {
+    return this.where({ downLimit });
+  }
+
+  tt_event_id(tt_event_id) {
+    return this.where({ tt_event_id });
+  }
 }
 
-module.exports = ChampionshipFilter
+module.exports = ChampionshipFilter;
