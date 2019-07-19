b0fd1d7ca80b54691192681ef1d53d97501eb4cb
Marlon Secundo <marlon_secundo@outlook.com>
2019-07-19 10:15:12 -0300

    Championship Filter Added

---
+++ app/ModelFilters/ChampionshipFilter.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const ModelFilter = use('ModelFilter')
+
+class ChampionshipFilter extends ModelFilter {
+
+}
+
+module.exports = ChampionshipFilter
--- app/Models/Championship.js
+++ app/Models/Championship.js
@@ -1,10 +1,12 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model');
 const { championship } = use('App/Utils/ModelsPath');
+const ChampionshipFilter = use('App/ModelFilters/ChampionshipFilter');
 
 class Championship extends Model {
   static boot() {
     super.boot();
+    this.addTrait('@provider:Filterable', ChampionshipFilter);
   }
 
   static columns() {
