97d44d940b023f74dde7d91a87c8b0cd427f01bb
John Victor <johnvictorio3@gmail.com>
2019-12-16 13:28:46 -0300

    fix: get cities by uf

--- app/Controllers/Http/StateController.js
+++ app/Controllers/Http/StateController.js
@@ -17,10 +17,18 @@ class StateController {
   }
 
   async show({ request }) {
-    const { id } = request.params;
+    const { id } = request.params; // id: UF do estado
+
+    const states = await axios.get(
+      'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+    );
+
+    const stateId = states.data
+      .filter((i) => i.sigla === id)
+      .map((i) => i.id)[0];
 
     const response = await axios.get(
-      `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${id}/municipios`
+      `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${stateId}/municipios`
     );
 
     const cities = response.data.map((item) => {
