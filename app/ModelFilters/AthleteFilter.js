5aa8c75ed3e014ba3387c3f221b9d95c41b9a8f0
Marlon Secundo <marlon_secundo@outlook.com>
2020-02-05 13:39:20 -0300

    get athletes by event_id filter

--- app/ModelFilters/AthleteFilter.js
+++ app/ModelFilters/AthleteFilter.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const ModelFilter = use('ModelFilter');
-
+const Database = use('Database');
 class AthleteFilter extends ModelFilter {
   club(club_id) {
     return this.where({ club_id });
@@ -10,6 +10,17 @@ class AthleteFilter extends ModelFilter {
   rating(rating) {
     const value = parseInt(rating, 10);
     return this.whereBetween('rating', [value - 100, value + 100]);
+  }
+
+  event(tt_event_id) {
+    const champSubquery = Database.from('championships')
+      .select('id')
+      .where({ tt_event_id });
+    const athlInsSubquery = Database.from('athlete_inscriptions')
+      .select('athlete_id')
+      .whereIn('championship_id', champSubquery);
+
+    return this.whereIn('id', athlInsSubquery);
   }
 }
 
